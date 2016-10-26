b6d0da431ee6f38dfb3f1539ef148d2106a7303c
Rafael Peralta Jr <rperaltajr@gmail.com>
2016-10-26 14:56:30 +0000

    Style errors and messages partials

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -18,7 +18,7 @@ class ArticlesController < ApplicationController
     @article = Article.new(article_params) # pass in a variable
     
     if @article.save
-      flash[:notice] = "Article was successfully created" # Flash message
+      flash[:success] = "Article was successfully created" # Flash message
       redirect_to article_path(@article) # according to `rake routes` which says the Prefix is 'article'
     else
       render 'new' # render 'new' template
@@ -28,7 +28,7 @@ class ArticlesController < ApplicationController
   def update
     @article = Article.find(params[:id]) # find 'id' from the params hash
     if @article.update(article_params) # update article according to 'id' from params hash
-      flash[:notice] = "Article was successfully updated"
+      flash[:success] = "Article was successfully updated"
       redirect_to article_path(@article)
     else
       render 'edit'
@@ -42,7 +42,7 @@ class ArticlesController < ApplicationController
   def destroy
     @article = Article.find(params[:id])
     @article.destroy
-    flash[:notice] = "Article was successfully deleted"
+    flash[:danger] = "Article was successfully deleted"
     redirect_to articles_path
   end
   
