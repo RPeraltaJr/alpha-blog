25bb35de8fc91c81fea8c2fed6c81bb909c0a621
Rafael Peralta Jr <rperaltajr@gmail.com>
2016-10-26 16:12:37 +0000

    Style articles listing page

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,6 +1,6 @@
 class Article < ActiveRecord::Base
   
   validates :title, presence: true, length: { minimum: 3, maximum: 50 } # enforces before creating object, ensures there is a title 
-  validates :description, presence: true, length: { minimum: 10, maximum: 300 }
+  validates :description, presence: true, length: { minimum: 10, maximum: 1000 }
   
 end
